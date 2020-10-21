0e4e73a2aece5c75cb8e1f4aa51831f278d1fe98
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-20 22:56:47 -0400

    die

--- src/com/margieblair/Die.java
+++ src/com/margieblair/Die.java
@@ -2,4 +2,20 @@ package com.margieblair;
 
 public class Die {
     //this is our die class for java
+    private int value;
+//    private int sides; DND would use more sided dies
+    //doesn't need to be dynamic for Yahtzee
+    private int sides = 6;
+
+    public Die() {
+        value = 1;
+    }
+    public Die(int sides) {
+        value = 1;
+        this.sides = sides;
+    }
+    //in the instance that we're getting a different
+    //type of die
+
+
 }
