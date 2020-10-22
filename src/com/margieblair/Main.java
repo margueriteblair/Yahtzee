11f9a73d8f4b126c97a9dc4b4ef943fa56c877a9
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-22 02:46:41 -0400

    put roll count in a while loop

--- src/com/margieblair/Main.java
+++ src/com/margieblair/Main.java
@@ -8,17 +8,24 @@ public class Main {
 	// write your code here
         //create a game loop!
         //lol i should actually learn how yahtzee works in order to do this
+        int count = 0;
         Random rand = new Random();
+        String player;
         YahtzeeConsole.welcome();
         Player myHand = new Player();
         myHand.roll(rand);
-        YahtzeeConsole.displayDice(myHand.getDice());
-        myHand.roll(rand, YahtzeeConsole.getChoices());
-        YahtzeeConsole.displayDice(myHand.getDice());
-        myHand.roll(rand, YahtzeeConsole.getChoices());
-        YahtzeeConsole.displayDice(myHand.getDice());
-        myHand.roll(rand, YahtzeeConsole.getChoices());
-        YahtzeeConsole.displayDice(myHand.getDice());
+        while (count < 3) {
+            YahtzeeConsole.displayDice(myHand.getDice());
+            myHand.roll(rand, YahtzeeConsole.getChoices());
+            count++;
+        }
+//        YahtzeeConsole.displayDice(myHand.getDice());
+//        myHand.roll(rand, YahtzeeConsole.getChoices());
+//        YahtzeeConsole.displayDice(myHand.getDice());
+//        myHand.roll(rand, YahtzeeConsole.getChoices());
+//        YahtzeeConsole.displayDice(myHand.getDice());
+//        myHand.roll(rand, YahtzeeConsole.getChoices());
+//        YahtzeeConsole.displayDice(myHand.getDice());
 
     }
 }
